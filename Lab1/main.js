2033c761ba5d8fbd9b24ba57e2d1f69346577b3a
masza221 <80392735+masza221@users.noreply.github.com>
2022-10-06 09:46:19 +0200

    edit on arr

--- Lab1/main.js
+++ Lab1/main.js
@@ -1,7 +1,4 @@
-let liczba1 = document.querySelector("#liczba1");
-liczba2 = document.querySelector('#liczba2');
-liczba3 = document.querySelector('#liczba3');
-liczba4 = document.querySelector('#liczba4');
+
 resultDiv = document.querySelector(".result");
 inputs = document.querySelectorAll(".inputs");
 
@@ -12,7 +9,9 @@ function calc() {
     })
     max= Math.max(...arrOfResults);
     min = Math.min(...arrOfResults);
-    sum= +liczba1.value + +liczba2.value + +liczba3.value + +liczba4.value;
+    sum= arrOfResults.reduce((result, number) => {
+        return result + number;
+      }, 0);
     av = sum / 4;
     resultDiv.innerHTML = `Max: ${max} Min:${min} Sum:${sum} Av:${av} `
 }
